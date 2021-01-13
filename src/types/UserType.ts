63dd00a18c7ff24372e2dc31f0d55f9b98dd0fd5
櫻木真彩 <sakuragimaaya@sakuragimakotoayanoMacBook-Air.local>
2021-01-13 23:44:56 +0900

    NORMAL2

---
+++ src/types/UserType.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export interface Mondai {
+    id: number;
+    name: string;
+    url: string;
+    isStart: false;
+}
+let normal: Mondai = { id: 1, name: 'mondai', url: 'https://mondai.com', isStart: false };
+
